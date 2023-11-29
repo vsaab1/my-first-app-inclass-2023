ce2d55e91d9f86a6ef405b3ff672c7a3c41cc5b0
vsaab1 <vs647@georgetown.edu>
2023-11-29 17:55:45 -0500

    created stocks dashboard

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,11 +1,11 @@
 
 # this is the "web_app/__init__.py" file...
-
+import os
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.unemployment_routes import unemployment_routes
-#from web_app.routes.stocks_routes import stocks_routes
+from web_app.routes.stocks_routes import stocks_routes
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
 
@@ -15,9 +15,10 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(unemployment_routes)
-    #app.register_blueprint(stocks_routes)
+    app.register_blueprint(stocks_routes)
     return app
 
 if __name__ == "__main__":
     my_app = create_app()
     my_app.run(debug=True)
+
